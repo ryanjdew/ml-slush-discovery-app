093f48176f28e21f3dc6ff17b3e1f4c96cbbe4ba
Ryan Dew <ryan.j.dew@gmail.com>
2016-01-22 15:53:32 -0700

    Clear out error message upon next successful call to endpoint that failed

--- ui/app/error/errorInterceptor.service.js
+++ ui/app/error/errorInterceptor.service.js
@@ -10,8 +10,18 @@
   ErrorInterceptor.$inject = ['$q', '$injector'];
   function ErrorInterceptor($q, $injector) {
     var errorInterceptor = {
+      // optional method
+      response: function(response) {
+        var source = response.config.method + ':' + response.config.url;
+        var messageBoardService = $injector.get('messageBoardService');
+        if (messageBoardService.message() && messageBoardService.source() === source) {
+          messageBoardService.message(null);
+        }
+        return response;
+      },
       responseError: function(rejection) {
         if (rejection.status === 500 || rejection.status === 400) {
+          var source = rejection.config.method + ':' + rejection.config.url;
           var messageBoardService = $injector.get('messageBoardService');
           var msg;
           if (rejection.data && rejection.data.errorResponse) {
@@ -24,7 +34,7 @@
               title: (rejection.status === 400) ? 'Bad Request' : 'Internal Server Error'
             };
           }
-          messageBoardService.message(msg);
+          messageBoardService.message(msg).source(source).type('error');
         }
         return $q.reject(rejection);
       }
--- ui/app/message-board/message-board.service.js
+++ ui/app/message-board/message-board.service.js
@@ -7,6 +7,14 @@
   MessageBoardService.$inject = ['$rootScope'];
   function MessageBoardService($rootScope) {
     var _message = null;
+    var _type = null;
+    var _source = null;
+
+    var service = {
+      message: message,
+      source: source,
+      type: type
+    };
 
     function message(msg) {
       if (msg === undefined) {
@@ -18,14 +26,29 @@
       } else {
         $rootScope.$broadcast('message-board:cleared');
       }
+      return service;
+    }
+
+    function type(typeOfMsg) {
+      if (typeOfMsg === undefined) {
+        return _type;
+      }
+      _type = typeOfMsg;
+      return service;
+    }
+
+    function source(src) {
+      if (src === undefined) {
+        return _source;
+      }
+      _source = src;
+      return service;
     }
 
     $rootScope.$on('$stateChangeSuccess', function() {
       message(null);
     });
 
-    return {
-      message: message
-    };
+    return service;
   }
 }());
