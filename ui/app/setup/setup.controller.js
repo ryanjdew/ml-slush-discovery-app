912797d9fb25fd8cb94d5bce53d179bbce901514
Ryan Dew <ryan.j.dew@gmail.com>
2016-03-10 14:56:42 -0700

    Allow reordering of constraints

--- ui/app/setup/setup.controller.js
+++ ui/app/setup/setup.controller.js
@@ -24,6 +24,23 @@
     var model = {};
     var mlSearch = searchFactory.newContext();
 
+    function moveArrayItem(array, old_index, new_index) {
+      while (old_index < 0) {
+          old_index += array.length;
+      }
+      while (new_index < 0) {
+          new_index += array.length;
+      }
+      if (new_index >= array.length) {
+          var k = new_index - array.length;
+          while ((k--) + 1) {
+              array.push(undefined);
+          }
+      }
+      array.splice(new_index, 0, array.splice(old_index, 1)[0]);
+      return array; // for testing purposes
+    }
+
     function updateSearchResults() {
       $scope.error = null;
       return mlSearch.setPageLength(5).search().then(
@@ -256,6 +273,9 @@
       },
       removeConstraint: function(index) {
         model.constraints.splice(index, 1);
+      },
+      reorderConstraint: function(index, newIndex) {
+        moveArrayItem(model.constraints, index, newIndex);
       },
       submitConstraints: function() {
         model.searchOptions.options.constraint = model.constraints;
