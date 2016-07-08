c02a79b2ce1c94186ce3f126ec446832cd566b84
Ryan Dew <ryan.j.dew@gmail.com>
2016-07-08 17:00:34 -0600

    Customized search results #5

---
+++ ui/app/setup/dialogs/newLabelPart.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+
+  var module = angular.module('app.setup');
+
+
+  module.controller('AddLabelPartCtrl', ['$uibModalInstance', '$scope', 'hasLabel', 'ServerConfig',
+    function($uibModalInstance, $scope, hasLabel, ServerConfig) {
+      $scope.hasLabel = hasLabel;
+      $scope.part = {
+              type: 'text',
+              value: ''
+            };
+      $scope.node = {};
+      $scope.add = function() {
+        if ($scope.part.type === 'element' || $scope.part.type === 'attribute') {
+          $scope.part.value = $scope.node.selectedNode;
+        }
+        $uibModalInstance.close($scope.part);
+      };
+    }
+  ]);
+
+  module.factory('newLabelPartDialog', [
+    '$uibModal',
+    function($uibModal) {
+      return function(hasLabel) {
+        return $uibModal.open({
+          templateUrl: '/ui/app/setup/dialogs/newLabelPart.html',
+          controller: 'AddLabelPartCtrl',
+          size: 'lg',
+          resolve: {
+            hasLabel: function() {
+              return hasLabel;
+            }
+          }
+        }).result;
+      };
+    }
+  ]);
+
+}());
