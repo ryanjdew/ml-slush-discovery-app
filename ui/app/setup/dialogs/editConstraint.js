004b389936fedb822f6f77c2f6a6cfad291727df
Ryan Dew <ryan.j.dew@gmail.com>
2016-07-22 14:48:06 -0600

    Make custom range constraints easier to configure

--- ui/app/setup/dialogs/editConstraint.js
+++ ui/app/setup/dialogs/editConstraint.js
@@ -11,14 +11,86 @@
    */
   module.controller('EditConstraintCtrl', [
     '$uibModalInstance', '$scope', 'ServerConfig',
-    'constraint', 'constraintType',
-    function ($uibModalInstance, $scope, ServerConfig, constraint, constraintType) {
+    'rangeIndexes', 'constraint', 'constraintType',
+    function ($uibModalInstance, $scope, ServerConfig, rangeIndexes, constraint, constraintType) {
       $scope.constraintTypes = _.filter(Object.keys(constraint), function(val) {
             return val !== 'name' && val !== 'annotation' && constraint.hasOwnProperty(val);
           });
+      $scope.rangeConstraintType = 'raw';
+      $scope.rangeIndexes = [];
+      angular.forEach(rangeIndexes, function(val) {
+        var rootKey = Object.keys(val)[0];
+        var copy = angular.copy(val[rootKey]);
+        copy.$label =
+          (copy['parent-localname'] ? copy['parent-localname'] + ' ' : '') +
+          (copy.localname || copy['field-name'] || copy['path-index']);
+        $scope.rangeIndexes.push(copy);
+      });
       $scope.dataTypes = ServerConfig.dataTypes();
       $scope.constraintType = constraintType || 'collection';
       $scope.constraint = constraint;
+
+      $scope.defaultRangeSettings = {
+            'facet': true,
+            'facet-option': [
+                  'limit=10',
+                  'frequency-order',
+                  'descending'
+                ]
+          };
+
+      function resetRangeConstraint() {
+        $scope.constraint.range = {
+          'facet':  $scope.constraint.range.facet || $scope.defaultRangeSettings.facet,
+          'facet-option':
+            $scope.constraint.range['facet-option'] || $scope.defaultRangeSettings['facet-option']
+        };
+      }
+
+      if ($scope.constraintType === 'range' && $scope.constraint.range) {
+        var rangeConstraint = $scope.constraint.range;
+        $scope.selectedRangeIndex = _.filter($scope.rangeIndexes, function(val) {
+          var matchedSelector = false;
+          if (val['parent-localname'] && rangeConstraint.element) {
+            matchedSelector = val['parent-localname'] === rangeConstraint.element.name;
+          }
+          if (val.localname && rangeConstraint.element && !rangeConstraint.attribute) {
+            matchedSelector = val.localname === rangeConstraint.element.name;
+          }
+          if (val.localname && rangeConstraint.attribute) {
+            matchedSelector = val.localname === rangeConstraint.attribute.name;
+          }
+          if (val['field-name'] && rangeConstraint.field) {
+            matchedSelector = val['field-name'] === rangeConstraint.field.name;
+          }
+          if (val['path-index'] && rangeConstraint['path-index']) {
+            matchedSelector = val['path-index'] === rangeConstraint['path-index'];
+          }
+          return
+          'xs:' + val['scalar-type'] === $scope.constraint.range.type &&
+           (val.collation === $scope.constraint.range.collation ||
+            !(val.collation || $scope.constraint.range.collation)) &&
+           matchedSelector;
+        })[0];
+        var computedBucket = rangeConstraint['computed-bucket'];
+        var bucket = rangeConstraint.bucket;
+        resetRangeConstraint();
+        $scope['computed-bucket'] = computedBucket;
+        $scope.constraint.range.bucket = bucket;
+        if (bucket) {
+          $scope.rangeConstraintType = 'bucket';
+        } else if (computedBucket) {
+          $scope.rangeConstraintType = 'computed-bucket';
+        }
+      }
+
+      $scope.$watch(function() { return $scope.rangeConstraintType;}, function() {
+        resetRangeConstraint();
+        if ($scope.rangeConstraintType && $scope.rangeConstraintType !== 'raw') {
+          $scope.constraint.range[$scope.rangeConstraintType] = [];
+        }
+      });
+
       $scope.save = function () {
         var newConstraint = {
           name: $scope.constraint.name,
@@ -52,6 +124,30 @@
           }
         });
         newConstraint[$scope.constraintType] = constraintDetails;
+        if ($scope.constraintType === 'range') {
+          var rangeIndex = $scope.selectedRangeIndex;
+          var rangeConstraint = newConstraint[$scope.constraintType];
+          rangeConstraint.type = 'xs:' + rangeIndex['scalar-type'];
+          rangeConstraint.collation = rangeIndex.collation;
+          if (rangeIndex.localname) {
+            rangeConstraint.element = {
+              'name': (rangeIndex['parent-localname'] || rangeIndex.localname),
+              'ns': (rangeIndex['parent-namespace-uri'] || rangeIndex['namespace-uri'])
+            };
+          }
+          if (rangeIndex['parent-localname']) {
+            rangeConstraint.attribute = {
+              'name': rangeIndex.localname,
+              'ns': rangeIndex['namespace-uri']
+            };
+          }
+          if (rangeIndex['field-name']) {
+            rangeConstraint.field = {
+              'name': rangeIndex['field-name'],
+              'collation': rangeIndex.collation
+            };
+          }
+        }
         $uibModalInstance.close(newConstraint);
       };
     }]);
@@ -66,31 +162,11 @@
   module.factory('EditConstraintDialog', [
     '$uibModal',
     function ($uibModal) {
-      return function (constraint) {
+      return function (rangeIndexes, constraint) {
         var modalConstraint = angular.extend({}, {
                   'name': '',
                   'annotation': '',
-                  'range': {
-                    'type': '',
-                    'collation' : 'http://marklogic.com/collation/',
-                    'facet': false,
-                    'element': {
-                      'ns': '',
-                      'name': ''
-                    },
-                    'attribute': {
-                      'ns': '',
-                      'name': ''
-                    },
-                    'field': {'name': ''},
-                    'json-property': '',
-                    'path-index': '',
-                    'bucket': [],
-                    'computed-bucket': [],
-                    'facet-option' : [],
-                    'range-option': [],
-                    'fragment-scope': 'documents'
-                  },
+                  'range': {},
                   'value': {
                     'type': null,
                     'element': {
@@ -141,6 +217,9 @@
             controller: 'EditConstraintCtrl',
             size: 'lg',
             resolve: {
+              rangeIndexes: function() {
+                return rangeIndexes;
+              },
               constraint: function() {
                 return modalConstraint;
               },
